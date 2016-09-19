b922bcb02ea9fc37f457168858d0ae79f7b71277
Olivier Brabanders <olivier.brabanders@gmail.com>
2016-09-19 17:57:18 +0200

    Make improvements on install.sh

--- install.sh
+++ install.sh
@@ -1,49 +1,47 @@
 #! /bin/sh
-# Switch to root
-su
-# Install i3
-pacman -S i3 --no-confirm
+# Install xfce desktop environment
+sudo pacman -S xfce4 --noconfirm
 # Install applications through Pacman
 echo "Installing Pacman applications..."
-pacman -S chromium --noconfirm # Favorite internet browser
-pacaur -S jre8-openjdk --no-confirm
-pacman -S pacgraph --no-confirm # Make nice graph of packages
-pacman -S zsh --no-confirm 
-pacman -S vlc --no-confirm # Watch movies, listen to music
-pacman -S youtube-dl --no-confirm # Download mp3
-pacman -S reflector --no-confirm # Update mirrorlist
-pacman -S slim --no-confirm # Login manager
+sudo pacman -S wget --noconfirm # For downloading simple stuff from the internet
+sudo pacman -S chromium --noconfirm # Favorite internet browser
+sudo pacman -S pacgraph --noconfirm # Make nice graph of packages
+sudo pacman -S zsh --noconfirm 
+sudo pacman -S vlc --noconfirm # Watch movies, listen to music
+sudo pacman -S youtube-dl --noconfirm # Download mp3
+sudo pacman -S reflector --noconfirm # Update mirrorlist
+sudo pacman -S slim --noconfirm # Login manager
 # Configure slim
 echo "Configure slim using https://wiki.archlinux.org/index.php/SLiM"
-pacman -S python --no-confirm
-pacman -S qbittorrent --no-confirm # Download P2P stuff
-pacman -S mcomix --no-confirm # Read comic books
+sudo pacman -S python --noconfirm
+sudo pacman -S qbittorrent --noconfirm # Download P2P stuff
+sudo pacman -S mcomix --noconfirm # Read comic books
 # Install favorite text editor
-pacman -S vim --no-confirm
+sudo pacman -S vim --noconfirm
 # Install favorite DBMS
-pacman -S postgresql --no-confirm
+sudo pacman -S postgresql --noconfirm
 # Start and enable postgresql service
 systemctl start postgresql.service
 systemctl enable postgresql.service
 echo "Finished installing Pacman applications..."
-exit
 echo "Retrieve sample .zshrc config"
-wget -O .zshrc http://git.grml.org/f/grml-etc-core/etc/zsh/zshrc # Retrieve sample .zshrc config
+wget -O ~/.zshrc http://git.grml.org/f/grml-etc-core/etc/zsh/zshrc # Retrieve sample .zshrc config
 
 echo "Installing AUR helper..."
 # Install AUR helper pacaur
 curl -s https://gist.githubusercontent.com/Tadly/0e65d30f279a34c33e9b/raw/pacaur_install.sh | bash
 # Install AUR programs
 echo "Installing programs from AUR..."
-pacaur -S dropbox
-pacaur -S pycharm-professional
-pacaur -S oh-my-zsh-git
+pacaur -Sa jdk8-openjdk --noconfirm
+pacaur -Sa dropbox --noconfirm
+pacaur -Sa pycharm-professional --noconfirm
+pacaur -Sa oh-my-zsh-git --noconfirm
 # Enable flash content for chromium
-pacaur -S chromium-pepper-flash
+pacaur -Sa chromium-pepper-flash --noconfirm
 # Enable Netflix for chromium
-pacaur -S chromium-widevine
+pacaur -Sa chromium-widevine --noconfirm
 # Watch movies
-pacaur -S popcorntime-bin
+pacaur -Sa popcorntime-bin --noconfirm
 echo "Finished installing..."
 echo "Make graph of installed packages..."
 pacgraph
@@ -51,7 +49,7 @@ pacgraph
 echo 'if [ -z "$DISPLAY" ] && [ "$(fgconsole)" -eq 1 ]; then
 	  exec startx
   fi' >> ~/.zprofile
-# Start i3 when Xorg starts
-echo 'exec i3' >> .xinitrc
+# Start xfce4 when Xorg starts
+echo 'exec startxfce4' >> ~/.xinitrc
 
 echo "Reboot to view changes!"
